1c6ade9f834ac58bdf334d0965a51e367925a51e
Jared Bates <computerwizjared@hotmail.com>
2016-10-01 18:29:13 -0500

    Add slider and move login stuff

---
+++ client/home.js
@@ -0,0 +1,14 @@
+Template.home.onRendered(function() {
+  var swiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: '.swiper-pagination',
+
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
+  })
+});
